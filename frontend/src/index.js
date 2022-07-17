a86f280477026b174e7979447b0bcce7b51f0f7c
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-07-17 21:15:08 +0300

    ref: packege.json

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 // import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
