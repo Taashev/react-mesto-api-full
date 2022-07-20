a59b5cc9c053c8a012785afe49f1a9443f6e30ce
Arsen Taashev <taashev.a.r92@yandex.ru>
2022-07-20 15:01:51 +0300

    fix: server port 3000

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 const app = express();
 
 // PORT
-const { PORT = 3001 } = process.env; //!!
+const { PORT = 3000 } = process.env;
 
 // reqest logger
 app.use(requestLogger);
